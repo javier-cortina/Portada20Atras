414f89e8c1d0137a6d4264ec5737bd63a4d4f1a8
jcortinaf <67200389+jcortinaf@users.noreply.github.com>
2021-05-01 12:33:00 +0200

    Update constants.py

--- lib/constants.py
+++ lib/constants.py
@@ -5,9 +5,6 @@ TIME_BACK = 20
 
 # Paths
 LOCAL_PATH = pathlib.Path().absolute()
-# LOCAL_PATH = '/home/pi/Documents/portada20atras'
-# KEY_PATH = LOCAL_PATH + '/lib/credentials.txt'
-# PORTADA_PATH = LOCAL_PATH + '/portadas'
 KEY_PATH = LOCAL_PATH / 'lib' / 'credentials.txt'
 PORTADA_PATH = LOCAL_PATH / 'portadas'
 
@@ -16,7 +13,3 @@ postElPais = '08:00'
 postABC = '09:00'
 postLaVanguardia = '10:00'
 postMundoDeportivo = '11:00'
-
-# Otros periodicos:
-# - Sport: 2011-Enero
-# - El Mundo: de pago
