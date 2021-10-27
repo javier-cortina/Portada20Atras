45652c5194b1929abd162f1a1afd63a70105348d
gluonafro <gluonafro@outlook.es>
2021-10-27 02:17:02 +0200

    add time magazine to bot

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 import sys
 import schedule
 import time
-from lib.constants import postElPais, postABC, postLaVanguardia, postMundoDeportivo, postElPaisSemanal
-from lib import elpais, abc, lavanguardia, mundodeportivo, elpaissemanal
+from lib.constants import postElPais, postABC, postLaVanguardia, postMundoDeportivo, postElPaisSemanal, postTimeMagazine
+from lib import elpais, abc, lavanguardia, mundodeportivo, elpaissemanal, timemagazine
 
 def closeApp():
     print('Job done for today! :D')
@@ -17,6 +17,7 @@ def main():
     schedule.every().day.at(postLaVanguardia).do(lavanguardia.tweetLaVanguardia)
     schedule.every().day.at(postMundoDeportivo).do(mundodeportivo.tweetMundoDeportivo)
     schedule.every().day.at(postElPaisSemanal).do(elpaissemanal.tweetElPaisSemanal)
+    schedule.every().day.at(postTimeMagazine).do(timemagazine.tweetTimeMagazine)
 
     # Define time to finalize script
     ep = postElPais.split(':')
@@ -24,11 +25,13 @@ def main():
     lv = postLaVanguardia.split(':')
     md = postMundoDeportivo.split(':')
     eps = postElPaisSemanal.split(':')
+    tm = postTimeMagazine.split(':')
     tmax = max(float(ep[0]) + float(ep[1])/60,
                float(ab[0]) + float(ab[1])/60,
                float(lv[0]) + float(lv[1])/60,
                float(md[0]) + float(md[1])/60,
-               float(eps[0]) + float(eps[1])/60)
+               float(eps[0]) + float(eps[1])/60),
+               float(tm[0]) + float(tm[1])/60)
     hmax = int(tmax)
     mmax = int((tmax - hmax)*60) + 5
     if mmax > 59:
